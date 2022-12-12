3b898c5417c5ad3026adda6b4c74491cef1ec0e5
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-12-12 21:01:58 +0300

    complete image genaration

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Static forlder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
